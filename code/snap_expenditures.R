d0148bace73e4f51a97b81d2f3ea1e2c20854b52
isabellav <isabella.velasquez@gatesfoundation.org>
2019-05-27 09:50:27 -0700

    create code for appendix a2

--- code/snap_expenditures.R
+++ code/snap_expenditures.R
@@ -1,54 +1,66 @@
+####################################
+# Extracting SNAP Expenditure Data #
+####################################
+
+
+# libraries ---------------------------------------------------------------
+
 library(tidyverse)
 library(viridis)
 library(tabulizer)
 library(janitor)
+
+# data --------------------------------------------------------------------
 
 # website: https://www.fns.usda.gov/snap/foods-typically-purchased-supplemental-nutrition-assistance-program-snap-households
 
 snap_pdf <-
   extract_tables("https://fns-prod.azureedge.net/sites/default/files/ops/SNAPFoodsTypicallyPurchased-Appendices.pdf")
 
-snap_appendix1 <- 
+# appendix 1 --------------------------------------------------------------
+
+# appendix a-1
+
+snap_appendix_a1 <- 
   snap_pdf %>%
   map(as_tibble) %>%
   map_df(~ slice(., -2)) %>% # create a dataframe
   mutate(col_dat = case_when(grepl("[0-9]", V2) ~ V2,
-                           grepl("[0-9]", V3) ~ V3,
-                           TRUE ~ "")) %>% 
+                             grepl("[0-9]", V3) ~ V3,
+                             TRUE ~ "")) %>% 
   filter(col_dat != "") %>% 
   select(V1, col_dat) %>%
-  slice(1:238) %>%  # only Appendix A
+  slice(1:238) %>%  # only Appendix A1
   separate(col_dat, " ", into = c("snap_rank", "snap_dollars_in_millions", "snap_pct_total_expenditures", "nonsnap_rank", "nonsnap_dollars_in_millions", "nonsnap_pct_total_expenditures")) %>% 
   rename(commodity = V1) %>%
   mutate_at(vars(snap_rank:nonsnap_pct_total_expenditures), funs(as.numeric(gsub(",|%|\\$", "", .)))) %>%
-  mutate_at(vars(contains("pct")), funs(./100))
+  mutate_at(vars(contains("pct")), list(~ ./100))
 
-snap_appendix1 <- write_csv(snap_appendix1, here::here("data", "snap_appendix1.csv"))
+# appendix a-2 
 
-snap_appendix1_tidy <-
-  snap_appendix1 %>% 
-  gather(category, value, -1) %>% 
-  mutate(household =
-           case_when(grepl("nonsnap", category) ~ "nonsnap",
-                               TRUE ~ "snap"),
-         column =
-           case_when(grepl("dollars_in_millions", category) ~ "dollars_in_millions",
-                                         grepl("pct_total_expenditures", category) ~ "pct_total_expenditures",
-                                         grepl("rank", category) ~ "rank")) %>% 
-  select(-category)
+snap_appendix_a2 <- 
+  snap_pdf %>%
+  map(as_tibble) %>%
+  map_df(~ slice(., -2)) %>% # create a dataframe
+  select(V1:V4) %>% 
+  slice(247:1277) %>%  # only Appendix A2 
+  rename(commodity = V1,
+         subcommodity = V2) %>% 
+  mutate(subcommodity = case_when(subcommodity == "" ~ V3,
+                                  TRUE ~ subcommodity),
+         col_dat = case_when(grepl("[0-9]", V3) ~ V3,
+                             grepl("[0-9]", V4) ~ V4,
+                             TRUE ~ "")) %>% 
+  select(-V3:-V4) %>% 
+  filter(col_dat != "") %>% 
+  separate(col_dat, "\\s+", into = c("snap_rank", "snap_dollars_in_millions", "snap_pct_total_expenditures", "nonsnap_rank", "nonsnap_dollars_in_millions", "nonsnap_pct_total_expenditures")) %>%  # different number of spaces
+  mutate_at(vars(snap_rank:nonsnap_pct_total_expenditures), funs(as.numeric(gsub(",|%|\\$", "", .)))) %>%
+  mutate_at(vars(contains("pct")), list(~ ./100))
 
-snap_appendix1_household <-
-  snap_appendix1_tidy %>%  
-  spread(column, value)
+# write data --------------------------------------------------------------
 
-# snap_appendix1_tidy %>% 
-#   filter(column == "rank") %>% 
-#   group_by(commodity) %>% 
-#   mutate(slope = (value[household == "snap"] - value[household == "nonsnap"])/(2-1)) %>% 
-#   ggplot(aes(x = household, y = value, group = commodity, colour = slope > 0)) +
-#   geom_line()
+snap_appendix_a1 <- 
+  write_csv(snap_appendix_a1, here::here("output", "snap_appendix_a1.csv"))
 
-
-snap_appendix1 %>% 
- ggplot(aes(x = snap_rank, y = nonsnap_rank)) +
- geom_point()
+snap_appendix_a2 <- 
+  write_csv(snap_appendix_a2, here::here("output", "snap_appendix_a2.csv"))
